18d95237fe4a38553bec4afd06e76769445aebef
farhanichlasulamal <farhan.ichlasulamal@student.uns.ac.id>
2018-06-01 15:50:00 +0700

    bug fix

--- Quick-Doodle/src/com/quickdoodle/game/GameGUI.java
+++ Quick-Doodle/src/com/quickdoodle/game/GameGUI.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;

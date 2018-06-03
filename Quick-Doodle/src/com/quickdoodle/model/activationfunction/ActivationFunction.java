547d5b4f200e8eecfd69cb3b1de3e9a377bf87af
TampanBerani <abrarfitrawan1@gmail.com>
2018-06-03 00:32:47 -0700

    Database

--- Quick-Doodle/src/com/quickdoodle/model/activationfunction/ActivationFunction.java
+++
@@ -1,9 +0,0 @@
-package com.quickdoodle.model.activationfunction;
-
-import org.ejml.simple.SimpleMatrix;
-
-public interface ActivationFunction {
-	
-	public SimpleMatrix apply(SimpleMatrix input, boolean derivative);
-	
-}
